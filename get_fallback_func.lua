6c9d579880d430bdbb725b89bfbbc7c58d426f06
Haunt <zxcggfdrdg@gmail.com>
2026-04-07 19:12:14 -0400

    Expose get_fallback_func to global scope
    Added global access to get_fallback_func.

--- get_fallback_func.lua
+++ get_fallback_func.lua
@@ -1,9 +1,16 @@
-local function get_fallback_func(t, f, fallback)
+local Global = (getgenv and getgenv())
+            or _G
+            or shared
+            or {}
+
+function get_fallback_func(t, f, fallback)
 	if type(f) == t then
 		return f
 	end
 
 	return fallback
 end
+
+Global.get_fallback_func = get_fallback_func
 
 return get_fallback_func
