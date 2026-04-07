0a209d875355da1ad119b099731643d85b019a90
Haunt <zxcggfdrdg@gmail.com>
2026-04-07 16:56:02 -0400

    Fix missing newline and return statement in Lua file

---
+++ get_instance_path.lua
@@ -0,0 +1,28 @@
+local function get_instance_path(Inst: Instance)
+	local success, result = pcall(function()
+		local x: string = Inst.Name
+		local current = Inst.Parent
+
+		while current do
+			x = tostring(current.Name) .. '/' .. x
+			if current == game then break end
+			current = current.Parent
+		end
+
+		return x
+	end)
+
+	if not success then
+		if result then
+			print("[-] Expected success on getInstancePath, got: " .. tostring(result))
+			return result
+		end
+		return nil
+	end
+
+	if success then
+		return result
+	end
+end
+
+return get_instance_path
